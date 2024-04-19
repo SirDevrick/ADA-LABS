2f0ed0ae10e585374a14a0a5bfdd86e63b01cf4b
Natzgun <itzlilouxx@gmail.com>
2024-04-19 09:38:25 -0500

    Solo funciona para una sola cantidad de datos

---
+++ GenData.c
@@ -0,0 +1,37 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void printData(int col) {
+  for (int i = 0; i < 25; i++) {
+    for (int j = 0; j < col; j++) {
+      printf("%d ", rand() % 1000 + 1);
+    }
+    printf("\n");
+  }
+}
+
+/* void genData(int col) { */
+/*   int **M = malloc(25 * sizeof(int *)); */
+/*   int data = 0; */
+/*   for (int i = 0; i < 25; i++) { */
+/*     M[i] = malloc(col * sizeof(int)); */
+/*     for (int j = 0; j < col; j++) { */
+/*       M[i][j] = rand() % 1000 + 1; */
+/*       data++; */
+/*     } */
+/*   } */
+/*   printData(M, col); */
+/* } */
+
+int main(int argc, char *argv[]) {
+  if (argc < 2) {
+    fprintf(stderr, "Enviar un argumento ./[app] [argument] \n");
+    exit(1);
+  }
+  int n = atoi(argv[1]);
+  printData(n);
+  /* genData(n); */
+  /* printf("%s", argv[1]); */
+  return 0;
+}
---
+++ InsertionSort.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#define MAX_SIZE 100
+
+/* int len(int *L) { */
+/*   int i = 0; */
+/*   while (*L) { */
+/*     L++; */
+/*     i++; */
+/*   } */
+/*   return i; */
+/* } */
+
+void insertionSort(int *L) {
+  for (int j = 1; j < MAX_SIZE; j++) {
+    int key = L[j];
+    int i = j - 1;
+    while (i >= 0 && L[i] > key) {
+      L[i + 1] = L[i];
+      i = i - 1;
+    }
+    L[i + 1] = key;
+  }
+}
+
+void avergae(int *L) {}
+
+int main() {
+  int c;
+  int arr[MAX_SIZE];
+  int cont = 0;
+  double dots[25];
+
+  FILE *fp = popen("gnuplot -persistent", "w");
+
+  clock_t start, end;
+
+  for (int i = 0; i < 25; i++) {
+
+    while (cont < MAX_SIZE && scanf("%d", &c)) {
+      arr[cont++] = c;
+    }
+
+    start = clock();
+    insertionSort(arr);
+    end = clock();
+    dots[i] = ((double)(end - start)) / CLOCKS_PER_SEC;
+
+    // Esto solo era para ver si estaba funcionando el ordenamiento
+    /* for (int i = 0; i < 10; i++) { */
+    /*   printf("%d ", arr[i]); */
+    /*   arr[i] = 0; */
+    /* } */
+    /* printf("\n"); */
+
+    cont = 0;
+  }
+  fclose(fp);
+  return 0;
+}
